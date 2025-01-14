2674d94dfde48a74c1addd6cd2355ebec4c6505c
jeewantha <upuljeewantha070@gmail.com>
2025-01-14 17:58:20 +0530

    connected the supabase backend with out React app

---
+++ src/services/apiCabins.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import supabase from "./supabase.js";
+
+export const getCabins = async () => {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+};
